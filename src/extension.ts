fe7bf3fc4a97eedcbd888c0ad43c031af1266fec
PZP-2 <hironin@foxmail.com>
2024-10-28 17:13:20 +0800

    add action.save command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let actions: any[] = [];
+
 export function activate(context: vscode.ExtensionContext) {
+    // 监听编辑器中的操作
+    context.subscriptions.push(
+        // 修改文本文件事件
+        vscode.workspace.onDidChangeTextDocument(event => {
+            logAction('changeTextDocument', event);
+            // logAction('changeTextDocument', event.document.uri.toString());
+        }),
+        // 保存文本文件事件
+        vscode.workspace.onDidSaveTextDocument(event => {
+            logAction('saveTextDocument', event);
+            logAction('saveTextDocument', event.uri.toString());
+        }),
+        // 切换文本编辑器事件
+        vscode.window.onDidChangeActiveTextEditor(editor => {
+            if (editor) {
+                logAction('changeActiveEditor', editor);
+                // logAction('changeActiveEditor', editor.document.uri.toString());
+            }
+        })
+    );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "plugin-test" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('plugin-test.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from plugin-test!');
-	});
-
-	context.subscriptions.push(disposable);
+    // 注册命令，保存动作数据
+    context.subscriptions.push(
+        vscode.commands.registerCommand('action.save', () => {
+            saveActions();
+        })
+    );
 }
 
-// This method is called when your extension is deactivated
+function logAction(action: string, event: any) {
+    actions.push({ action, event, timestamp: new Date().toLocaleString('zh-CN')});
+}
+
+function saveActions() {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+        vscode.window.showErrorMessage('No workspace folder found.');
+        return;
+    }
+    const filePath = path.join(workspaceFolders[0].uri.fsPath, 'actions.json');
+    fs.writeFileSync(filePath, JSON.stringify(actions, null, 2));
+    vscode.window.showInformationMessage(`Actions saved to ${filePath}`);
+}
+
 export function deactivate() {}
